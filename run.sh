ba0cf865c060dd1faf349ad63d45acc61099e12f
AlamHasabie <39056147+AlamHasabie@users.noreply.github.com>
2020-12-01 16:43:23 +0700

    Delete run.sh

--- run.sh
+++
@@ -1,3 +0,0 @@
-bin/hadoop com.sun.tools.javac.Main ../TriangleCounting/*.java
-jar cf ../TriangleCounting/triangle-counting.jar ../TriangleCounting/*.class
-bin/hadoop jar ../TriangleCounting/triangle-counting.jar TriangleCount ../TriangleCounting/data/sample ../TriangleCounting/data/output
